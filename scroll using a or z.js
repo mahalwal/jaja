069875b182146c7cd73e63210ac7687b403cb667
Manish Mahalwal <manish16054@iiitd.ac.in>
2021-01-24 04:48:08 +0530

    Create scroll using a or z.js

---
+++ scroll using a or z.js
@@ -0,0 +1,16 @@
+/* 
+Scroll using the a/z key after deleting the overlaying elements of a soft paywall
+*/
+
+
+function KeyPress(e) {
+  var evtobj = window.event? event : e
+  if (evtobj.keyCode == 90) {
+    window.scrollBy(0, 100) 
+  }
+  if (evtobj.keyCode == 65) {
+    window.scrollBy(0, -100) 
+  }
+}
+
+document.onkeydown = KeyPress;
